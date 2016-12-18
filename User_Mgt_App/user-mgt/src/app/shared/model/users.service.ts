171913b318a9e1bc3e3e692225ffec1e695b538e
wvanheemstra <willem@vanheemstrasystems.com>
2016-12-18 20:55:34 +0100

    Create dummy users inside users service, and a get users method

--- User_Mgt_App/user-mgt/src/app/shared/model/users.service.ts
+++ User_Mgt_App/user-mgt/src/app/shared/model/users.service.ts
@@ -1,8 +1,29 @@
 import { Injectable } from '@angular/core';
 
+var users = [
+    {
+        $key: "37894941d90b4e88a8754cc5976a9b0e",
+        firstname: "John",
+        lastname: "Doe"    
+    },
+    {
+        $key: "cba2b67adecc45fa8e7c90de6ab0e898",
+        firstname: "Jane",
+        lastname: "Doe" 
+    }
+];
+
 @Injectable()
 export class UsersService {
 
-  constructor() { }
+  private users; 
+
+  constructor() {
+    this.users = users;
+  }
+
+  getUsers() {
+    return this.users;
+  }
 
 }
