5de0b1fdeadedca4fa9a00463101051d2acd56b3
wvanheemstra <willem@vanheemstrasystems.com>
2016-12-18 20:11:57 +0100

    Create shared users service, and provide

--- User_Mgt_App/user-mgt/src/app/app.module.ts
+++ User_Mgt_App/user-mgt/src/app/app.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+import { UsersService } from './shared/model/users.service';
+
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 
@@ -16,7 +18,7 @@ import { HomeComponent } from './home/home.component';
     FormsModule,
     HttpModule
   ],
-  providers: [],
+  providers: [UsersService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
