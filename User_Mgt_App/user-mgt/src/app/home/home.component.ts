43627e653d82ca99eef61381f616fb80041d87fc
wvanheemstra <willem@vanheemstrasystems.com>
2016-12-19 19:37:29 +0100

    Add User model and get users to home component

--- User_Mgt_App/user-mgt/src/app/home/home.component.ts
+++ User_Mgt_App/user-mgt/src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../shared/model/user';
 import { UsersService } from '../shared/model/users.service';
 
 @Component({
@@ -9,10 +10,12 @@ import { UsersService } from '../shared/model/users.service';
 })
 export class HomeComponent implements OnInit {
 
+  users: User[];  
+    
   constructor(private usersService: UsersService) { 
-  
-  }
-
+    this.users = this.usersService.getUsers();
+  }    
+      
   ngOnInit() {
   }
 
