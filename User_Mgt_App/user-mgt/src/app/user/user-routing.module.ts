d8ddfa5f7004d0c6f0c00366aefb674abfaadc92
wvanheemstra <willem@vanheemstrasystems.com>
2016-12-20 17:33:00 +0100

    Add a route for 'users' to user-routing module

--- User_Mgt_App/user-mgt/src/app/user/user-routing.module.ts
+++ User_Mgt_App/user-mgt/src/app/user/user-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule }             from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { UserListComponent }  from '../user-list/user-list.component';
 import { UserDetailComponent }  from '../user-detail/user-detail.component';
 
 const userRoutes: Routes = [
+  { path: 'users', component: UserListComponent },
   { path: 'user/:id', component: UserDetailComponent }
 ];
 
