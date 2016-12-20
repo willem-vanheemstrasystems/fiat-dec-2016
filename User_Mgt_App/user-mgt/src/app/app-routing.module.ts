1146d378a38e31b71712abd14bbc42e1c4cf60b2
wvanheemstra <willem@vanheemstrasystems.com>
2016-12-20 17:40:08 +0100

    Create an app-routing module in 'app' directory

--- User_Mgt_App/user-mgt/src/app/app-routing.module.ts
+++ User_Mgt_App/user-mgt/src/app/app-routing.module.ts
@@ -2,12 +2,9 @@ import { NgModule }             from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { UserListComponent }    from './user-list/user-list.component';
-//import { UserDetailComponent }  from './user-detail/user-detail.component';
 
 const appRoutes: Routes = [
-  { path: '', component: UserListComponent }//,
-//  { path: 'users', component: UserListComponent },
-//  { path: 'user/:id', component: UserDetailComponent }
+  { path: '', component: UserListComponent }
 ];
 
 @NgModule({
