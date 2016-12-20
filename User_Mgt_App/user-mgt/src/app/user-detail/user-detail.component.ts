4cbcc13d08e8c3e8f3124afd5ab647d80db47902
wvanheemstra <willem@vanheemstrasystems.com>
2016-12-20 18:36:56 +0100

    Import routing services and switchMap in user-detail component

--- User_Mgt_App/user-mgt/src/app/user-detail/user-detail.component.ts
+++ User_Mgt_App/user-mgt/src/app/user-detail/user-detail.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, Params } from '@angular/router';
+import 'rxjs/add/operator/switchMap';
 
 @Component({
   selector: 'app-user-detail',
