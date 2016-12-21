8fc2bc1612492405c643267299b63ae5d20f8103
wvanheemstra <willem@vanheemstrasystems.com>
2016-12-21 10:15:33 +0100

    Inject services, get param of id in user-detail component

--- User_Mgt_App/user-mgt/src/app/user-detail/user-detail.component.ts
+++ User_Mgt_App/user-mgt/src/app/user-detail/user-detail.component.ts
@@ -1,17 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute, Params } from '@angular/router';
 import 'rxjs/add/operator/switchMap';
+import { UsersService } from '../shared/model/users.service';
+import { User } from '../shared/model/user';
 
 @Component({
-  selector: 'app-user-detail',
-  templateUrl: './user-detail.component.html',
-  styleUrls: ['./user-detail.component.css']
+    selector: 'app-user-detail',
+    templateUrl: './user-detail.component.html',
+    styleUrls: ['./user-detail.component.css']
 })
+
 export class UserDetailComponent implements OnInit {
 
-  constructor() { }
+    user: User;
+    
+    constructor(
+        private route: ActivatedRoute,
+        private router: Router,
+        private service: UsersService
+    ) { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+        this.route.params
+            // (+) converts string 'id' to a number
+            .switchMap((params: Params) => this.service.getUser(+params['id']))
+            .subscribe((user: User) => this.user = user);
+    }
 
 }
