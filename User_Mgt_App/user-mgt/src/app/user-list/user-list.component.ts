c803b1fb2a3c290e9c316c84df4b53d091916ded
wvanheemstra <willem@vanheemstrasystems.com>
2016-12-21 12:32:11 +0100

    Add routing services, switchMap and Observable to user-detail component

--- User_Mgt_App/user-mgt/src/app/user-list/user-list.component.ts
+++ User_Mgt_App/user-mgt/src/app/user-list/user-list.component.ts
@@ -1,29 +1,33 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Router, ActivatedRoute, Params } from '@angular/router';
+import 'rxjs/add/operator/switchMap';
+import { Observable } from 'rxjs/Observable';
 import { User } from '../shared/model/user';
 import { UsersService } from '../shared/model/users.service';
 
 @Component({
-  selector: 'app-user-list',
-  templateUrl: './user-list.component.html',
-  styleUrls: ['./user-list.component.css'],
-  providers: [UsersService]
+    selector: 'app-user-list',
+    templateUrl: './user-list.component.html',
+    styleUrls: ['./user-list.component.css'],
+    providers: [UsersService]
 })
+
 export class UserListComponent implements OnInit {
 
-  users: User[];  
-    
-  constructor(
-    private router: Router,
-    private usersService: UsersService) { 
-    this.users = this.usersService.getUsers();
-  }    
-      
-  ngOnInit() {
-  }
+    users: User;
 
-  onSelect(user: User) {
-    this.router.navigate(['/user', user.id]);
-  }  
-    
+    constructor(
+        private router: Router,
+        private route: ActivatedRoute,
+        private usersService: UsersService) {
+        this.users = this.usersService.getUsers();
+    }
+
+    ngOnInit() {
+    }
+
+    onSelect(user: User) {
+        this.router.navigate(['/user', user.id]);
+    }
+
 }
